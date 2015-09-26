a271aa61e4ba25f68bc02d7c9fce0a9db15f31fc
Philipp Bogensberger <philipp@crate.io>
2015-09-26 17:10:15 +0200

    added history with scrolling

--- texty/screen.py
+++ texty/screen.py
@@ -5,7 +5,7 @@ from texty.layers.background import Background
 DEFAULT_INPUT = 'Eingabe: '
 FONT_SIZE = 15
 HIS_WIDTH = 240
-HIS_FONT_SIZE = 13
+HIS_FONT_SIZE = 11
 
 
 def width():
@@ -20,15 +20,16 @@ class MainScreen():
 
     def __init__(self, callback):
         cocos.director.director.init(resizable=True, autoscale=False, width=1024, height=800)
-        self.screen = Screen()
         self.history = History()
+        self.screen = Screen(self.history)
         self.background = Background()
         self.main_scene = cocos.scene.Scene(
             self.background,
             self.screen,
             self.history,
-            KeyDisplay(callback)
+            KeyDisplay(callback, self.history)
         )
+        self.main_scene = cocos.scene.Scene(self.screen, self.history, KeyDisplay(callback, self.history))
 
     def run(self):
         cocos.director.director.run(self.main_scene)
@@ -41,12 +42,14 @@ class Screen(cocos.layer.Layer):
 
     is_event_handler = True
 
-    def __init__(self):
+    def __init__(self, history):
         super(Screen, self).__init__()
+        self.history = history
         self.label = None
         self.redraw_label()
 
     def text(self, text):
+        self.history.add_text(text)
         self.label.element.text = text
 
     def on_resize(self, width, height):
@@ -70,26 +73,56 @@ class Screen(cocos.layer.Layer):
 
 class History(cocos.layer.ScrollableLayer):
 
+    is_event_handler = True
+
     def __init__(self):
         super(History, self).__init__()
-        self.label = cocos.text.Label('',
+        self.label = None
+        self.redraw_label()
+
+    def add_text(self, text):
+        self.label.element.text += text + '\n'
+        self.autoscroll()
+
+    def on_resize(self, width, height):
+        self.redraw_label()
+
+    def redraw_label(self):
+        text = None
+        if self.label:
+            text = self.label.element.text
+            self.remove(self.label)
+        else:
+            text = ''
+        self.label = cocos.text.Label(text,
                                       font_name='Times New Roman',
                                       font_size=HIS_FONT_SIZE,
                                       anchor_x='left',
                                       anchor_y='top',
                                       multiline=True,
                                       width=HIS_WIDTH,
-                                      x=0, y=5)
+                                      x=10, y=height() - 10)
         self.add(self.label)
+        self.autoscroll()
+
+    def on_mouse_scroll(self, x, y, delta_x, delta_y):
+        self.label.position = (self.label.position[0],
+                               self.label.position[1] + delta_y)
+
+    def autoscroll(self):
+        if self.label.element.content_height > height() - 10:
+            self.label.position = (0, self.label.element.content_height - height())
+            print(self.label.position)
 
 
 class KeyDisplay(cocos.layer.Layer):
 
     is_event_handler = True
 
-    def __init__(self, callback):
+    def __init__(self, callback, history):
         super(KeyDisplay, self).__init__()
         self.callback = callback
+        self.history = history
 
         self.text_label = cocos.text.Label("", x=HIS_WIDTH + 10, y=5, font_size=FONT_SIZE)
         self.text = DEFAULT_INPUT
@@ -115,5 +148,6 @@ class KeyDisplay(cocos.layer.Layer):
 
     def submit(self):
         self.callback(self.text)
+        self.history.add_text(self.text[len(DEFAULT_INPUT):])
         self.text = DEFAULT_INPUT
         self.update_text()
