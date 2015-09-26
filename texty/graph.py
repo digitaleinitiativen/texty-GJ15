499d9eba80cc85e78159af724d46b2dcc0000f08
Mathias Fussenegger <f.mathias@zignar.net>
2015-09-26 13:55:45 +0200

    add more actions in level1

--- texty/graph.py
+++ texty/graph.py
@@ -18,6 +18,15 @@ class Node:
 
     def do(self, action):
         print('Aktion: {0}'.format(action))
+        if not action or not action.action:
+            return self
+
+        if hasattr(self, action.action):
+            result = getattr(self, action.action)(action.obj)
+            if result:
+                return result
+            return self
+
         if action in self.actions:
             action_result  = self.actions[action]
             if isinstance(action_result, Node):
@@ -49,13 +58,51 @@ n3 = Node(
 
 
 n2 = Node(
-    description='Das essen schmeckt gut',
+    description='',
     actions={
         Action('schnurren', None): n3
     }
 )
 
-n1 = Node(
+
+class N1(Node):
+    def __init__(self, description, actions, move_to=None):
+        super().__init__(description, actions, move_to)
+
+    def schnurren(self, obj=None):
+        self.description = '''
+Oh, dies dünkt den Herr Katz ganz annehmbar. Molback, da fängt der Seckel doch
+glatt an zu schnurren. Wie praktisch, dass ihm ein Laserpointerlicht aufgeht.
+'''
+
+    def _activate_portal(self, obj):
+        self.actions[Action('beamen', 'Portal')] = n2
+        self.description = 'wie praktisch ein glitzerndes Portal.'
+
+    def fressen(self, obj):
+        if obj == 'filet':
+            self.description = '''
+nomnomnomnomnomnomnomnom, ploetzlich, ein greller Blitz am Himmel, ein
+flammender Meteor stuertzt zurueck aus der Zukunft zur Erde, da stand er, der
+Leibhaftige, der einzig wahre Herr Katz. Zurueckgekehrt aus der Zukunft um sich
+selbst vor der Kastration zu retten. "Miau, sagt der Gegenwarts Herr Katz"
+"Salve du Stricher" sag der Zukunfts Herr Katz, "Woher kommst denn du" sagt der
+Gegenwarts Herr Katz, "aus der Zukunft" sagt der Zukunfts Herr Katz "Mau" sagt
+der Gegenwarts Herr Katz "Mau" sagt der Zukunfts Herr Katz "Ja wo sind denn
+Eier, ja wo sind sie denn, ja wo, ja wo sind sie denn?" fragt der Gegenwarts
+Herr Katz "weg" antwortet der Zukunfts Herr Katz "Warum" sagt der Gegenwarts
+Herr Katz "Hinweis: Filet am Vorabend + Tierarzt + Morgen = ?" "Mau" sagt der
+Gegenwarts Herr Katz, der die Weisheit wirklich nicht mit dem Löffel gefresssen
+hat" "Ich bin den Zukunfts Ich, gekommen aus der Zukunft, um dich davor zu
+bewahren morgen kastriert zu werden" sagt der Zukunfts Herr Katz "Wie
+aufmerksam" sagt der Gegenwarts Herr Katz "Flieht ihr Narren! Am besten durch
+glitzerne Katzenklappen Wink mit dem Zaunpfal" sagt der Zukunfts Herr Katz und
+verschwindet in einer dicken, nach Fisch stinkenden Rauchwolke.
+    '''
+            self.actions[Action('aktivieren', 'Portal')] = self._activate_portal
+
+
+n1 = N1(
     description='''
 Es war einmal vor langer Zeit, gestern, der ehrenwerte Herr Katz. Herr Katz war
 ein Lauser, also nicht sonderlich verlaust, aber dafuer hatte er Lausbefall der
@@ -65,10 +112,6 @@ versonnen seine Eier und jagte von Zeit zu Zeit seinen eigenen Schwanz.
 Erfolglos. Ja, so war er, der Herr Katz. Gestern Abend also, da saß er so in
 seinem Wohnzimmer. Orientierungslos. Hoechste Zeit sich umzusehen.''',
     actions={
-        Action('schnurren', None): change_description('''
-Oh, dies dünkt den Herr Katz ganz annehmbar. Molback, da fängt der Seckel doch
-glatt an zu schnurren. Wie praktisch, dass ihm ein Laserpointerlicht aufgeht.
-'''),
         Action('umsehen', None): change_description('''
 Der werte Herr Katze sieht sich um und entdeckt eine mysteriös funkelnde
 Katzenklappe, ein hässliches Potpourri, eine Katzentoilette, einen Stapel
