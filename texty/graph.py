1fa55725f23f12047359c121b31968bbf01847aa
Mathias Fussenegger <f.mathias@zignar.net>
2015-09-26 13:26:44 +0200

    add option to change description

--- texty/graph.py
+++ texty/graph.py
@@ -19,11 +19,20 @@ class Node:
     def do(self, action):
         print('Aktion: {0}'.format(action))
         if action in self.actions:
-            next_node = self.actions[action]
-            next_node.move_to()
-            return next_node
+            action_result  = self.actions[action]
+            if isinstance(action_result, Node):
+                action_result.move_to()
+                return action_result
+            elif callable(action_result):
+                action_result(self)
+                return self
         else:
             return self
+
+def change_description(description):
+    def do_change(node):
+        node.description = description
+    return do_change
 
 
 node_game_over = Node(
@@ -47,10 +56,26 @@ n2 = Node(
 )
 
 n1 = Node(
-    description=('Du siehst ein Gummiball und ein Fisch.\n'
-                 'Was tust du?'),
+    description='''
+Es war einmal vor langer Zeit, gestern, der ehrenwerte Herr Katz. Herr Katz war
+ein Lauser, also nicht sonderlich verlaust, aber dafuer hatte er Lausbefall der
+uebleren Sorte. Und er war etwas schwer von Begriff und auch in uebrigen
+Belangen. So starrte er tagein, tagaus ins Leere, leckte sich gelegentlich
+versonnen seine Eier und jagte von Zeit zu Zeit seinen eigenen Schwanz.
+Erfolglos. Ja, so war er, der Herr Katz. Gestern Abend also, da saß er so in
+seinem Wohnzimmer. Orientierungslos. Hoechste Zeit sich umzusehen.''',
     actions={
-        Action('fressen', 'gummiball'): node_game_over,
-        Action('fressen', 'fisch'): n2
+        Action('schnurren', None): change_description('''
+Oh, dies dünkt den Herr Katz ganz annehmbar. Molback, da fängt der Seckel doch
+glatt an zu schnurren. Wie praktisch, dass ihm ein Laserpointerlicht aufgeht.
+'''),
+        Action('umsehen', None): change_description('''
+Der werte Herr Katze sieht sich um und entdeckt eine mysteriös funkelnde
+Katzenklappe, ein hässliches Potpourri, eine Katzentoilette, einen Stapel
+frisch gebügelter Wäsche, ein Filet auf einem geblümelten Villeroy und Boch
+Teller'''),
+        Action('schlafen', None): change_description('''
+zzzzz, mau, zzzzz, mau, zzzzz, mau, zzzz, mau, wuff "oh I forgot how to cat",
+zzzz, mau, zzzz, mau'''),
     }
 )
