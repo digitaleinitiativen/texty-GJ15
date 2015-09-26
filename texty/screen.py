d8a72501311926d5e1d41f732f07421be25cb416
Philipp Bogensberger <philipp@crate.io>
2015-09-26 16:10:23 +0200

    changed font size and default screen size

--- texty/screen.py
+++ texty/screen.py
@@ -2,6 +2,9 @@ import pyglet
 import cocos
 
 DEFAULT_INPUT = 'Eingabe: '
+FONT_SIZE = 15
+HIS_WIDTH = 240
+HIS_FONT_SIZE = 13
 
 
 def width():
@@ -15,9 +18,10 @@ def height():
 class MainScreen():
 
     def __init__(self, callback):
-        cocos.director.director.init(resizable=True, autoscale=False)
+        cocos.director.director.init(resizable=True, autoscale=False, width=1024, height=800)
         self.screen = Screen()
-        self.main_scene = cocos.scene.Scene(self.screen, KeyDisplay(callback))
+        self.history = History()
+        self.main_scene = cocos.scene.Scene(self.screen, self.history, KeyDisplay(callback))
 
     def run(self):
         cocos.director.director.run(self.main_scene)
@@ -50,11 +54,26 @@ class Screen(cocos.layer.Layer):
             text = ''
         self.label = cocos.text.Label(text,
             font_name='Times New Roman',
-            font_size=15,
+            font_size=FONT_SIZE,
             anchor_x='left', anchor_y='top', multiline=True, width=width() - 10)
-        self.label.position = 10, height() - 10
+        self.label.position = HIS_WIDTH + 10, height() - 10
         self.add(self.label)
         self.draw()
+
+
+class History(cocos.layer.ScrollableLayer):
+
+    def __init__(self):
+        super(History, self).__init__()
+        self.label = cocos.text.Label('',
+                                      font_name='Times New Roman',
+                                      font_size=HIS_FONT_SIZE,
+                                      anchor_x='left',
+                                      anchor_y='top',
+                                      multiline=True,
+                                      width=HIS_WIDTH,
+                                      x=0, y=5)
+        self.add(self.label)
 
 
 class KeyDisplay(cocos.layer.Layer):
@@ -65,7 +84,7 @@ class KeyDisplay(cocos.layer.Layer):
         super(KeyDisplay, self).__init__()
         self.callback = callback
 
-        self.text_label = cocos.text.Label("", x=5, y=5)
+        self.text_label = cocos.text.Label("", x=HIS_WIDTH + 10, y=5, font_size=FONT_SIZE)
         self.text = DEFAULT_INPUT
 
         self.update_text()
@@ -89,4 +108,3 @@ class KeyDisplay(cocos.layer.Layer):
         self.callback(self.text)
         self.text = DEFAULT_INPUT
         self.update_text()
-
