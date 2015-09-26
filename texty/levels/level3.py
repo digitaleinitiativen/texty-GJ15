d87b4cf378d55dcad8e790ea800b49c815501f39
dielauserin <sara@lauserin.com>
2015-09-26 21:37:53 +0200

    Update level3.py

--- texty/levels/level3.py
+++ texty/levels/level3.py
@@ -11,7 +11,7 @@ class Level(Node):
 Auf die Plätze, Feuer, fertig, Flamme, schlafende-Katze-wird-von-Staubsauger-überrascht-Geschwindigkeit, los! 
 So war der Herr Katz also durch die zweite Klappe gebeamt. Als seine
 Augen sich an das Neongeflacker gewöhnt haben, findet er sich in einem
-runden Raum mit 4 Ecken wieder. In jeder eine Katzenklappe. Ein Reisebüro also. '''
+runden Raum mit 4 Ecken wieder. In jeder eine Katzenklappe. Ein Reisebüro also. Und was sieht man hier sonst so? '''
         super().__init__(description,
                          actions={},
                          objects={})
@@ -35,16 +35,16 @@ Er starrt in das katzenhafte Gesicht einer Katze. Fräulein Travelpussy
 liest er auf dem  Namensschild. Macht Sinn. Denn Fräulein
 Travelpussy mag Traveling und Puzzeln. “MauMauMauMauMau” mauzt Herr Katz.
 “Mau” erwidert Pussy. “Mau” sagt Herr Katz nickend.
-Nachdem jetzt alles geklärt ist, nun zu den Spielregeln. Zur Auswahl stehen 4
-Klappen. Doch ob er wirklich richtig geht, weiß er erst, wenn das
+Nachdem jetzt alles geklärt ist, nun zu den Spielregeln. Eine Frage, 4 Antworten. 
+Jede Antwort führt in eine der vier Klappen. Doch ob er wirklich richtig geht, weiß er erst, wenn das
 Funkelglitzerkatzenklappenportallicht angeht. Und Fräulein Travelpussy,
 die alte Showmasterin, kennt die Antwort, macht aber einen auf Sphinx. Mahlzeit. 
 Nun zum Rätsel. Eine Katze will sich auf den Mars
 einschmuggeln, muss aber an einer räudigen Türsteherkatze vorbei. Er beobachtet. 
-Als erstes kommt eine Katze. Räudigen Türsteherkatze sagt:"16", worauf die Katze schlicht:"8"
+Als erstes kommt eine Katze. Räudige Türsteherkatze sagt:"16", worauf die Katze schlicht:"8"
 sagt. Dann kommt eine Katze. Räudige Türsteherkatze sagt:"28"
 und Katze sagt:"14". Als eine Katze kommt, sagt räudige Türsteherkatze:"8" und bekommt als Antwort:"4". Alle dürfen passieren.
-Ach so, das ist ja einfach, denkt die Katze und antwortet auf Katzes Frage:"12" lässig "6" und wird umgehend
-verhaftet. Was hätte er wohl sagen müssen? a)5 b)5 c)29038 oder d)10'''
+Ach so, das ist ja einfach, denkt die Katze und antwortet auf räudige Katzes Frage:"12" lässig "6" und wird umgehend
+verhaftet. Was hätte er wohl sagen müssen? a)6 b)5 c)29038 oder d)10'''
 
 level3 = Level()
