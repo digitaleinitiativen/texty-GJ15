6c5d8fd56944b059d453a180af3da932eb78cfad
Mathias Fussenegger <f.mathias@zignar.net>
2015-09-26 10:30:58 +0200

    add initial buildout

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+requirements = [
+    'cocos2d'
+]
+
+setup(
+    name='texty',
+    install_requires=requirements
+)
