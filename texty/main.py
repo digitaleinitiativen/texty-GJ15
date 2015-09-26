77f6ee20218f8365c5e3a2d56d8c45d9978fbf4b
Philipp Bogensberger <philipp@crate.io>
2015-09-26 11:16:40 +0200

    pep8

--- texty/main.py
+++ texty/main.py
@@ -5,15 +5,14 @@ import cocos
 
 class HelloWorld(cocos.layer.Layer):
     def __init__(self):
-        super( HelloWorld, self ).__init__()
-
+        super(HelloWorld, self).__init__()
 
 
 def main():
     cocos.director.director.init()
     h = HelloWorld()
-    main_scene = cocos.scene.Scene (h)
-    cocos.director.director.run (main_scene)
+    main_scene = cocos.scene.Scene(h)
+    cocos.director.director.run(main_scene)
 
 
 if __name__ == '__main__':
