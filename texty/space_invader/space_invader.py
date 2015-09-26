f0ce291440e87589e4eac5b20a51bc5cb5916237
pde <peter.derfoeldi@triplein.at>
2015-09-26 22:04:09 +0200

    Workaround for asteroids

--- texty/space_invader/space_invader.py
+++ texty/space_invader/space_invader.py
@@ -8,7 +8,8 @@ from background import Background
 import pyglet
 from pyglet.window import key
 from pyglet.window.key import KeyStateHandler
-
+import cocos.euclid as eu
+import cocos.collision_model as cm
 
 
 def width():
@@ -36,6 +37,10 @@ class Animation(cocos.layer.Layer):
         self.spaceship = cocos.sprite.Sprite('spaceship.png')
         self.spaceship.position = (width()/2, 100)
         self.spaceship.scale = 1
+        self.spaceship.name = "SPACESHIP"
+        self.spaceship.cshape = cm.CircleShape(eu.Vector2(self.spaceship.position[0], self.spaceship.position[1]), self.spaceship.width/2)
+
+        self.collision_manager = cm.CollisionManagerBruteForce()
         
         self.bullet_move = None
         self.bullet_move_duration = 3
@@ -83,28 +88,25 @@ class Animation(cocos.layer.Layer):
         i = 0
         j = 0
         while i < 10:
-            self.asteroids.append([])
             while j < 5:
-                self.asteroids[i].append(self.init_asteroid(i,j))
+                self.init_asteroid(i,j)
                 j += 1
             j = 0
             i += 1
 
         
     def init_asteroid(self,x,y):
-        if y == 0:
-            asteroid = cocos.sprite.Sprite('asteroids/small/a300{0:02}.png'.format(x))
-        elif y == 1:
-            asteroid = cocos.sprite.Sprite('asteroids/small/a400{0:02}.png'.format(x))
-        elif y == 2:
-            asteroid = cocos.sprite.Sprite('asteroids/small/b100{0:02}.png'.format(x))
-        else:
-            asteroid = cocos.sprite.Sprite('asteroids/small/b400{0:02}.png'.format(x))
+        asteroid = cocos.sprite.Sprite('asteroids/small/a300{0:02}.png'.format(x))
         asteroid.scale = 1
         asteroid.position = asteroid.width/2 + x * asteroid.width, height() - y * asteroid.height - asteroid.height/2 
-        self.add(asteroid,z=0)
+        asteroid.cshape = cm.AARectShape(eu.Vector2(asteroid.position[0], asteroid.position[1]), asteroid.width/2,asteroid.height/2)
+        asteroid.name = "ASTEROID"
         asteroid.do(Repeat(RotateBy(180,2)))
-        return asteroid
+        
+        if y == 4 :
+            self.collision_manager.add(asteroid)
+            self.asteroids.append(asteroid)
+            self.add(asteroid,z=0)
 
         
 
@@ -112,7 +114,10 @@ class Animation(cocos.layer.Layer):
         bullet = cocos.sprite.Sprite('asteroids/small/a10000.png')
         bullet.scale = 1
         bullet.position = (self.spaceship.position[0],self.spaceship.position[1]+self.spaceship.height)
+        bullet.cshape = cm.CircleShape(eu.Vector2(bullet.position[0], bullet.position[1]), bullet.width/2)
         bullet.do(Repeat(MoveBy((0,self.bullet_move_step),self.bullet_move_duration)))
+        bullet.name = "BULLET"
+        self.collision_manager.add(bullet)
         self.shots += 1
         self.add(bullet, z=1)
         self.bullets.append(bullet)
@@ -122,6 +127,7 @@ class Animation(cocos.layer.Layer):
             if bullet.position[1] > height():
                 self.remove(bullet)
                 self.bullets.remove(bullet)
+                self.collision_manager.remove_tricky
 
     def spaceship_position(self):
         if self.spaceship.position[0] <= 0:
@@ -168,17 +174,40 @@ class Animation(cocos.layer.Layer):
 
 
     def update(self,dt):
+
+        for hit in self.collision_manager.iter_all_collisions():
+              if hit[0].name != hit[1].name:
+                if hit[0].name == "BULLET" and hit[0] in self.bullets:
+                    self.bullets.remove(hit[0])
+                    self.remove(hit[0])
+                if hit[1].name == "BULLET" and hit[1] in self.bullets:
+                    self.bullets.remove(hit[1])
+                    self.remove(hit[1])
+                if hit[0].name == "ASTEROID" and hit[0] in self.asteroids:
+                    self.asteroids.remove(hit[0])
+                    self.remove(hit[0])
+                if hit[1].name == "ASTEROID" and hit[1] in self.asteroids:
+                    self.asteroids.remove(hit[1])
+                    self.remove(hit[1])
+
+        self.collision_manager.clear()
         self.remove_bullets()
         self.spaceship_position()
         self.move_spaceship()
-        if self.shots > 30:
-            self.exit(True)
+
         if pyglet.window.key.SPACE in self.keys_pressed :
             self.shot()
             self.keys_pressed.remove(pyglet.window.key.SPACE)
         if pyglet.window.key.Q in self.keys_pressed :
             self.exit(False)
+
+        for obj in self.asteroids:
+            self.collision_manager.add(obj) 
+        for obj in self.bullets:
+            self.collision_manager.add(obj) 
        
+        if not self.asteroids:
+            self.exit(True)
 
 class SpaceInvader():
     def __init__(self,exit_callback):
