b0fe25d5b9e60ef2f5e1c45f5e0c69b9e478ec02
dielauserin <sara@lauserin.com>
2015-09-26 18:57:34 +0200

    Update level1.py

--- texty/levels/level1.py
+++ texty/levels/level1.py
@@ -19,10 +19,10 @@ class Level(Node):
     def umsehen(self, obj=None):
         if not obj:
             self.description = '''
-Der werte Herr Katze sieht sich um und entdeckt eine mysteriös funkelnde
+Der werte Herr Katz sieht sich um und entdeckt eine mysteriös funkelnde
 Katzenklappe, ein hässliches Potpourri, eine Katzentoilette, einen Stapel
 frisch gebügelter Wäsche, ein Filet auf einem geblümelten Villeroy und Boch
-Teller'''
+Teller.'''
         else:
             self.description = ('Du siehst {0} an aber findest es langweilig.'
                                 'Du bist hungrig'.format(obj))
@@ -67,12 +67,13 @@ wenn er von A nach B kommen wollen würde.
 level1 = Level(
     description='''
 Es war einmal vor langer Zeit, gestern, der ehrenwerte Herr Katz. Herr Katz war
-ein Lauser, also nicht sonderlich verlaust, aber dafuer hatte er Lausbefall der
-uebleren Sorte. Und er war etwas schwer von Begriff und auch in uebrigen
-Belangen. So starrte er tagein, tagaus ins Leere, leckte sich gelegentlich
-versonnen seine Eier und jagte von Zeit zu Zeit seinen eigenen Schwanz.
-Erfolglos. Ja, so war er, der Herr Katz. Gestern Abend also, da saß er so in
-seinem Wohnzimmer. Orientierungslos. Hoechste Zeit sich umzusehen.''',
+ein Lauser, nicht sonderlich verlaust, dafür hatte er Lausbefall der
+übleren Sorte. Und er war etwas schwer von Begriff. Also eigentlich strohdumm.
+So starrte er tagein, tagaus ins Leere, leckte sich gelegentlich
+versonnen seine Eier, schlief vor sich hin, fraß alles und jagte von Zeit zu Zeit seinen eigenen Schwanz.
+Erfolglos. Wenn er keinen Plan hat, also nicht mehr weiter weiß, dann schnurrt Herr Katz.
+Ja, so ist er, der Herr Katz. Gestern Abend also, da saß er so in
+seinem Wohnzimmer. Orientierungslos. Höchste Zeit sich umzuschauen.''',
     actions={
         Action('schlafen', None): change_description('''
 zzzzz, mau, zzzzz, mau, zzzzz, mau, zzzz, mau, wuff "oh I forgot how to cat",
